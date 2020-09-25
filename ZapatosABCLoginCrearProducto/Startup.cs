60b8f2c61e44f793b74555ea7b1e0d3e5ea6ae1b
Andres Hernandez <afhernandezg@outlook.com>
2020-09-25 07:27:30 -0500

    Add project files.

---
+++ ZapatosABCLoginCrearProducto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZapatosABCLoginCrearProducto.Startup))]
+namespace ZapatosABCLoginCrearProducto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
